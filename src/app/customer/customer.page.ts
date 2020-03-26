09c67793be4b3e524d72d8c63050309ccc33292d
luksanaSut <nan.luksana@gmail.com>
2020-03-26 13:38:23 +0700

    modified file customer gat data all in nex

--- src/app/customer/customer.page.ts
+++ src/app/customer/customer.page.ts
@@ -7,10 +7,12 @@ import { Subscription, from } from 'rxjs';
 
 interface resultData {
   cus_id: string,
-  ncd_customer: string,
-  ncd_services: string,
-  ncd_ip_wan: string,
+  ncd_customer: any[],
+  ncd_services: any[],
+  ncd_ip_wan: any[],
   ncd_device: any[],
+  ncd_interface: any[],
+  ncd_vrf: any[],
   
 }
 
@@ -26,9 +28,11 @@ export class CustomerPage implements OnInit {
   public ip_addr3: string = ''; 
   public ncd_device: any[] = [];
   public cus_id: string = ''; 
-  public ncd_customer: string = ''; 
-  public ncd_services: string = ''; 
-  public ncd_ip_wan: string = ''; 
+  public ncd_customer: any[] = []; 
+  public ncd_services: any[] = [];
+  public ncd_ip_wan: any[] = [];
+  public ncd_interface: any[] = [];
+  public ncd_vrf: any[] = [];
 
   sub: Subscription;
 
@@ -48,17 +52,27 @@ export class CustomerPage implements OnInit {
       this.ip_addr3 + this.service_id
     ).subscribe((result: resultData) => {
       this.cus_id = result.cus_id;
-      this.ncd_customer = result.ncd_customer;
-      this.ncd_device = result.ncd_device;
-      this.ncd_services = result.ncd_services;
-      this.ncd_ip_wan = result.ncd_ip_wan;
+      this.ncd_customer = Object.entries(result.ncd_customer);
+      this.ncd_device = Object.entries(result.ncd_device);
+      this.ncd_services = Object.entries(result.ncd_services);
+      this.ncd_ip_wan = Object.entries(result.ncd_ip_wan);
+      this.ncd_interface = Object.entries(result.ncd_interface);
+      this.ncd_vrf = Object.entries(result.ncd_vrf);
       // for (const [key, value] of Object.entries(result)) {
       //   let type = typeof value;
+        
       //   if(type === "object"){
-
+      //     for (const [k, va] of Object.entries(value)) {
+      //       // console.log(va);
+      //     }
+      //   }else{
+          
       //   }
-      //   // console.log(`${key} ${value}`);
+        
       // }
+
+
+      console.log(this.ncd_device)
       
      
 
